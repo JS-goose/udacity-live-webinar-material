70f33c7b654a81c69b9da47ef47d93d14dfdc8a7
Jonathan Sexton <joncsexton@gmail.com>
2019-04-27 18:15:09 -0500

    Final adjustments and comments

---
+++ Week 7 - Managing Focus for Accessibility/scripts.js
@@ -0,0 +1,8 @@
+// I didn't get this to work in our webinar but I challenge you to give it a go!  
+//  If you get it to work make sure you share it with me :D
+
+const buttonOne = document.querySelector('#button-one');
+
+document.onload(function() {
+    buttonOne.focus();
+})
