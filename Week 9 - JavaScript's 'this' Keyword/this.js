f83d202e3a14c2155a1cd024f1058975af5a652e
Jonathan Sexton <joncsexton@.com>
2019-05-11 17:26:28 -0500

    added examples for this keyword

--- Week 9 - JavaScript's 'this' Keyword/this.js
+++ Week 9 - JavaScript's 'this' Keyword/this.js
@@ -1,18 +1,41 @@
-'use strict'
+// 'use strict'
 function Obj() {
-    this.ham = 'yes',
-    this.bacon = 'yes',
-    this.legs = '4',
-    this.makeNoise = function() {
-        this.bacon = 'please'
-        console.log('oink!');
-    };
-
+  (this.ham = "yes"),
+    (this.bacon = "yes"),
+    (this.legs = "4"),
+    (this.makeNoise = function() {
+      this.bacon = "please";
+      console.log("oink!");
+    });
 }
+const one = { one: "one" };
 const testObj = new Obj();
 
 function pigNoise() {
-    this.b = 'foo';
+  this.b = "foo";
 }
 pigNoise.bind(this)();
 
+// These examples below have been borrowed from MDN https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/this#The_bind_method
+//this inside of function
+function thisFunction() {
+  return this;
+}
+
+console.log(thisFunction() === window);
+
+//Arrow Functions
+var globalObject = this;
+var foo = () => this;
+console.log(foo() === globalObject);
+
+// Call as a method of an object
+var obj = { func: foo };
+console.log(obj.func() === globalObject); // true
+
+// Attempt to set this using call
+console.log(foo.call(obj) === globalObject); // true
+
+// Attempt to set this using bind
+foo = foo.bind(obj);
+console.log(foo() === globalObject); // true
